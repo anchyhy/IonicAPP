6df6d9ab318b54b8f76526b86adc8fa265eaec83
Anqi Shao <anqi.shao@concur.com>
2018-09-18 09:13:18 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { Camera } from '@ionic-native/camera';
+
+import { File } from '@ionic-native/file';
+import { Transfer, TransferObject} from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { HttpModule  } from '@angular/http';
+
+import {SpeechRecognition} from '@ionic-native/speech-recognition'; 
 @NgModule({
   declarations: [
     MyApp,
@@ -21,6 +29,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -32,9 +41,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
+    Camera,
     StatusBar,
     SplashScreen,
+    File,
+    Transfer,
+    FilePath,
+    SpeechRecognition,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+
+export class AppModule {
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,70 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController} from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
+import { File } from '@ionic-native/file';
+import { Transfer, TransferObject} from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { Http  , ResponseOptions , Headers  , URLSearchParams } from '@angular/http';
+import {SpeechRecognition} from '@ionic-native/speech-recognition'; 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public base64Image: string;
+  constructor(private speech: SpeechRecognition, private camera: Camera, public navCtrl: NavController, private transfer:Transfer, private file: File, private http:Http) { }
 
-  constructor(public navCtrl: NavController) {
+  async takePicture(){
+  this.camera.getPicture().then((imageData) => {
+   let base64Image = 'data:image/jpeg;base64,' + imageData;
 
-  }
+   console.log(base64Image);
+   var currentName = imageData.substr(imageData.lastIndexOf('/') + 1);
+   var correctPath = imageData.substr(0, imageData.lastIndexOf('/') + 1);
+   console.log('currentName' + currentName);
+   console.log('correctPath' + correctPath);
+   var options = {
+                  fileKey: "file",
+                  fileName: currentName,
+                  chunkedMode: false,
+                  mimeType: "multipart/form-data",
+                  params : {'file': currentName}
+                };
+   var url = "http://10.171.171.95:8080/receipt-recognition/api/v1/recognition/image/hackathon";
+   const fileTransfer: TransferObject = this.transfer.create();
+   fileTransfer.upload(imageData, url, options).then(response => {
+              console.log('response:' + JSON.stringify(response));
+              //console.log(response.data.response.request_id);
+            }, err => {
+            console.log('uploading error');
+            console.log(JSON.stringify(err));
+            });
+   console.log('uploading is done....');
+  
+  var url1 = "http://10.171.171.95:8080/receipt-recognition/api/v1/recognition/document/";
+  console.log('url1:' + url1);
+  this.http.get(url1,{  
+    params: {  
+        "request_id":"MTI2N2FiYzItZTdlMy00ZWU2LTkzMjgtMzM0MWQ2MjJkNzE0LyJvY3JUcmFuc2xhdGVkVGV4dC50eHQi"  
+    }  
+}).subscribe(response =>{  
+      console.log(response);
+})  
+}, (err) => {
+   // Handle error
+   console.log("there is an error");
+  });
+}
+
+
+async speechIt():Promise<boolean> {
+  
+  const isAvailabel = await this.speech.isRecognitionAvailable();
+  console.log(isAvailabel);
+  return isAvailabel;
+}
+
+
 
 }
